80bab7b74abaff774aa93fed8fd74fa4dbe85325
sudo-yama <tao_2349@hotmail.com>
2024-09-06 17:17:23 +0700

    fix commit wrong branch

--- exercise5/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "yama-s3"
-    key    = "terraform/backend"
-    region = "us-east-1"
-  }
-}
--- exercise6/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "yama-s3"
-    key    = "terraform/backend_exercise6"
-    region = "us-east-1"
-  }
-}
